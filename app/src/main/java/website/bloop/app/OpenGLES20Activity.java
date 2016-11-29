aaeb193be5dfe0fa7d0cbe9a13ad450acb484311
Philip Seger <seger.philip@gmail.com>
2016-11-28 19:53:29 -0500

    ADD: start of drawing and animations in opengl

---
+++ app/src/main/java/website/bloop/app/OpenGLES20Activity.java
@@ -0,0 +1,42 @@
+package website.bloop.app;
+
+import android.app.Activity;
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+/**
+ * Created by philip on 11/28/16.
+ */
+
+public class OpenGLES20Activity extends Activity {
+
+    private GLSurfaceView mGLView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity.
+        mGLView = new MyGLSurfaceView(this);
+        setContentView(mGLView);
+    }
+
+    class MyGLSurfaceView extends GLSurfaceView {
+
+        private final MyGLRenderer mRenderer;
+
+        public MyGLSurfaceView(Context context){
+            super(context);
+
+            // Create an OpenGL ES 2.0 context
+            setEGLContextClientVersion(2);
+
+            mRenderer = new MyGLRenderer();
+
+            // Set the Renderer for drawing on the GLSurfaceView
+            setRenderer(mRenderer);
+        }
+    }
+}
