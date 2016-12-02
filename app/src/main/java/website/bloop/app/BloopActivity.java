53b67db90ae406f686e3286912dd0add9b5eebf8
wolfd <2dawolf@gmail.com>
2016-12-02 14:14:09 -0500

    Add bootprints.

--- app/src/main/java/website/bloop/app/BloopActivity.java
+++ app/src/main/java/website/bloop/app/BloopActivity.java
@@ -5,13 +5,16 @@ import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
-import android.widget.TextView;
 
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.GroundOverlay;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.mikepenz.aboutlibraries.Libs;
@@ -19,7 +22,9 @@ import com.mikepenz.aboutlibraries.LibsBuilder;
 import com.patloew.rxlocation.RxLocation;
 import com.tbruyelle.rxpermissions2.RxPermissions;
 
-import butterknife.BindView;
+import java.util.LinkedList;
+import java.util.List;
+
 import butterknife.ButterKnife;
 import io.reactivex.Observable;
 
@@ -31,13 +36,22 @@ public class BloopActivity extends FragmentActivity implements OnMapReadyCallbac
 
     private GoogleMap mMap;
 
-    @BindView(R.id.overlay_text_view)
-    TextView mOverlayTextView;
+    private List<GroundOverlay> mBootprintLocations;
+    private int mTotalSteps;
+    private BitmapDescriptor mLeftBootprint;
+    private BitmapDescriptor mRightBootprint;
+    private Location mCurrentLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bloop);
+
+        // we're going to be removing the first element a lot, LinkedList will be good
+        mBootprintLocations = new LinkedList<>();
+
+        mLeftBootprint = BitmapDescriptorFactory.fromResource(R.drawable.bootprint_left);
+        mRightBootprint = BitmapDescriptorFactory.fromResource(R.drawable.bootprint_right);
 
         ButterKnife.bind(this);
 
@@ -86,16 +100,70 @@ public class BloopActivity extends FragmentActivity implements OnMapReadyCallbac
         //noinspection MissingPermission
         mRxLocation.location().updates(locationRequest)
                 //TODO: we might want to clean this data before passing it on
+                .doOnEach(location -> mCurrentLocation = location.getValue())
                 .doOnEach(location -> updateMapCenter(location.getValue()))
-                .doOnEach(location -> updateOverlayTextView(location.getValue()))
+                .doOnEach(location -> updateMyLocation(location.getValue()))
                 .subscribe();
     }
 
-    private void updateOverlayTextView(Location location) {
-        if (mOverlayTextView != null) {
-            mOverlayTextView.setText(
-                    String.format("%s %s", location.getLatitude(), location.getLongitude())
+    private void updateMyLocation(Location location) {
+        if (mBootprintLocations == null) {
+            return;
+        }
+
+        LatLng latLng = new LatLng(
+                location.getLatitude(),
+                location.getLongitude()
+        );
+
+        if (mBootprintLocations.size() == 0) {
+            // first time, place left and right prints
+            placeBootprint(latLng, 0);
+            placeBootprint(latLng, 0);
+
+            return;
+        }
+
+        if (mBootprintLocations.size() >= 1) {
+            // we have a bootprint, only place another if we're far enough away from it
+            final LatLng lastLatLng = mBootprintLocations.get(
+                    mBootprintLocations.size() - 1
+            ).getPosition();
+
+            double deltaLat = lastLatLng.latitude - latLng.latitude;
+            double deltaLong = lastLatLng.longitude - latLng.longitude;
+
+            double mercatorDistance = Math.sqrt(
+                    Math.pow(deltaLat, 2) + Math.pow(deltaLong, 2)
             );
+
+            if (mercatorDistance > 0.0000895f) {
+                placeBootprint(latLng, (float) Math.atan2(deltaLong, deltaLat));
+            }
+        }
+    }
+
+    private void placeBootprint(LatLng latLng, float bearing) {
+        if (mMap != null) {
+            final BitmapDescriptor bootprint;
+            // pick left/right print
+            if (mTotalSteps++ % 2 == 0) {
+                bootprint = mLeftBootprint;
+            } else {
+                bootprint = mRightBootprint;
+            }
+
+
+            final GroundOverlay overlay = mMap.addGroundOverlay(
+                    new GroundOverlayOptions().position(
+                            latLng,
+                            10
+                    ).bearing(
+                            (float) (bearing / Math.PI * 180d) + 180f
+                    ).image(bootprint)
+            );
+
+            mBootprintLocations.add(overlay);
         }
     }
 
