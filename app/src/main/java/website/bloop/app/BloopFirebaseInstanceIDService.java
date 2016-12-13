b078257ace7941cca62e1c2dae5ff20c33219cfb
Sam Myers <sam@students.olin.edu>
2016-12-13 18:42:18 -0500

    Adding Firebase.

---
+++ app/src/main/java/website/bloop/app/BloopFirebaseInstanceIDService.java
@@ -0,0 +1,12 @@
+package website.bloop.app;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * TODO: Add a class header comment!
+ */
+
+public class BloopFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "BloopFirebaseIIDService";
+}
---
+++ app/src/main/java/website/bloop/app/BloopFirebaseMessagingService.java
@@ -0,0 +1,12 @@
+package website.bloop.app;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * TODO: Add a class header comment!
+ */
+
+public class BloopFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "BloopFirebaseMsgService";
+}
